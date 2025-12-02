5e5187beb0527d0fb8b16de400ae6b708acb2eae
d3nik <firftrif23@gmail.com>
2025-12-02 23:55:13 +0200

    6.0 Clothes pages creation
    – imported new images;
    – clothes cards images and descriptions changed;
    – created and styled new pages for clothes;
    – a few another changes;

---
+++ js/index.js
@@ -0,0 +1,6 @@
+function changeLargeImage(imageUrl) {
+    var largeImage = document.getElementById('largeImage');
+    largeImage.src = imageUrl;
+    document.querySelector('.active-border').classList.remove('active-border');
+    event.target.classList.add('active-border');
+}
